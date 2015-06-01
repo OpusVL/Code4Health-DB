83f4f871fb4ac5cd87a7ed6cd2b9470262065090
Colin Newell <colin@opusvl.com>
2015-06-01 11:39:24 +0100

    Updated documentation

--- lib/Code4Health/DB.pm
+++ lib/Code4Health/DB.pm
@@ -6,7 +6,7 @@ use warnings FATAL => 'all';
 
 =head1 NAME
 
-Code4Health::DB - The great new Code4Health::DB!
+Code4Health::DB - Data layer for the Code4Health system.
 
 =head1 VERSION
 
@@ -17,49 +17,10 @@ Version 0.01
 our $VERSION = '0.01';
 
 
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Code4Health::DB;
-
-    my $foo = Code4Health::DB->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
 OpusVL, C<< <support at opusvl.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-code4health-db at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Code4Health-DB>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
 
 
 =head1 SUPPORT
@@ -67,29 +28,6 @@ automatically be notified of progress on your bug as I make changes.
 You can find documentation for this module with the perldoc command.
 
     perldoc Code4Health::DB
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Code4Health-DB>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Code4Health-DB>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Code4Health-DB>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Code4Health-DB/>
-
-=back
 
 
 =head1 ACKNOWLEDGEMENTS
