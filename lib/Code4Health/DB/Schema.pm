98ffebe4d34afc11db08cb9e92368af3c9aef06e
Colin Newell <colin@opusvl.com>
2015-06-01 12:40:24 +0100

    Added schema class

---
+++ lib/Code4Health/DB/Schema.pm
@@ -0,0 +1,26 @@
+package Code4Health::DB::Schema;
+
+use base qw/DBIx::Class::Schema/;
+
+__PACKAGE__->load_namespaces();
+
+1;
+
+=head1 NAME
+
+Code4Health::DB::Schema
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 ATTRIBUTES
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 OpusVL.
+
+This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+
+=cut
