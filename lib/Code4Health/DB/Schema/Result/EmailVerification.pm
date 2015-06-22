08965cc6af2d71dadb5c823859598133caea362d
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2015-06-22 13:43:05 +0100

    Add POD for Result::EmailVerification

--- lib/Code4Health/DB/Schema/Result/EmailVerification.pm
+++ lib/Code4Health/DB/Schema/Result/EmailVerification.pm
@@ -18,3 +18,33 @@ column email => {
 
 
 1;
+
+=head1 NAME
+
+Code4Health::DB::Schema::Result::EmailVerification
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 ATTRIBUTES
+
+=head2 hash
+
+The generated unique hash
+
+=head2 expiry
+
+When the hash is no longer valid
+
+=head2 email
+
+Email address it is for (where the reset email was sent)
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 OpusVL.
+
+This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+
+=cut
