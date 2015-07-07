9a3d5d4498f30d55d31cc3966f43a3f28415b3ef
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2015-07-07 13:58:36 +0100

    Add help text for deploy script

--- deploy.pl
+++ deploy.pl
@@ -12,7 +12,7 @@ use Getopt::Long qw(:config gnu_getopt);
 my %option;
 my $command = shift;
 
-GetOptions(\%option, 'target=f');
+GetOptions(\%option, 'target=f', 'help|h|?');
 
 my $schema = Code4Health::DB::Schema->connect({
     dsn => 'dbi:Pg:host=localhost;dbname=code4health',
@@ -28,6 +28,7 @@ my %command = (
     upgrade => \&upgrade,
     deploy => \&deploy,
     'set-version' => \&set_version,
+    help => \&help,
 );
 
 $command{$command || 'upgrade'}->($dh);
@@ -107,6 +108,49 @@ sub deploy {
 
     # Install everything.
     $dh->install({
-        version => $version
+        version => $schema->schema_version
     });
+}
+
+sub help {
+    say <<EOHELP
+Usage: 
+    $0 <command> [--target n] [--help|-h|-?]
+
+Default command is upgrade.
+
+Works with DBIx::Class::DeploymentHandler to upgrade or downgrade the database
+between versions.
+
+To go between versions, source files (YAML format) are created by DH to
+represent the database at each version.  Then it can diff these structures and
+produce SQL for the various versions.
+
+Options:
+    --target n
+        Targets version n. If omitted, targets the schema_version in
+        Code4Health::DB::Schema
+
+Commands:
+    init
+        Sets up DeploymentHandler's table and YAML files for the current
+        version. You can't use --target for this because that would require you
+        to actually have the schema files checked out to that version.
+
+    upgrade
+        Upgrades to the targetted version.
+
+    downgrade
+        Downgrades to the targetted version, with variable success.
+
+    set-version
+        Tells DeploymentHandler that the database is currently on the targetted
+        version.
+
+    deploy
+        Deploys the entire database at the current version. --target is ignored
+        here because the code is used to produce this; you have to check out the
+        right version of the files to deploy that version.
+
+EOHELP
 }
