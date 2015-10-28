444e0a2daba557d25791dbeeec644e3ac2c29070
Colin Newell <colin@opusvl.com>
2015-10-28 08:22:06 +0000

    Added pod

--- lib/Code4Health/DB/Schema/ResultSet/Community.pm
+++ lib/Code4Health/DB/Schema/ResultSet/Community.pm
@@ -19,3 +19,26 @@ sub by_name
 
 1;
 
+
+=head1 NAME
+
+Code4Health::DB::Schema::ResultSet::Community
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 active
+
+=head2 by_name
+
+=head1 ATTRIBUTES
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 OpusVL.
+
+This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+
+=cut
