ea614ab3ed01a3b29416735922ede61af40f0e78
Colin Newell <colin@opusvl.com>
2015-11-24 10:51:21 +0000

    Added default data

--- mig/sys_info.sql
+++ mig/sys_info.sql
@@ -4,3 +4,8 @@ CREATE TABLE sys_info (
   comment text,
   PRIMARY KEY (name)
 );
+INSERT INTO sys_info (name, value, comment) VALUES ('signup.supporter.email', '"Thanks for signing up as a Supporter member, [% name %]!"', NULL);
+INSERT INTO sys_info (name, value, comment) VALUES ('signup.supporter.subject', '"Welcome as a Supporter!"', NULL);
+INSERT INTO sys_info (name, value, comment) VALUES ('signup.community.email', '"Thanks for applying to form a new Community, [% name %]!\r\n\r\nWe will review your application and be in touch shortly."', NULL);
+INSERT INTO sys_info (name, value, comment) VALUES ('signup.community.subject', '"We''ve received your application to form a new Community"', NULL);
+
